0351f7e8daf9edf18c419574e0ebf346ed239b57
Harsh Ubriani <harsh.ubriani@kantar.com>
2024-07-29 22:55:06 +0100

    add alb permission

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,17 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_s3_bucket" "newrelic_lambda_bucket" {
+resource "aws_s3_bucket" "kpp_lb_and_lambda_bucket" {
   bucket = var.s3_bucket  # Make sure this name is unique
   acl    = "private"
   
   tags = {
-    Name        = "LBLogs"    
+    Name        = "alb-logs-bucket"    
   }
 }
 
 resource "aws_s3_bucket_object" "lambda_zip" {
-  bucket = aws_s3_bucket.newrelic_lambda_bucket.bucket
+  bucket = aws_s3_bucket.kpp_lb_and_lambda_bucket.bucket
   key    = var.s3_key
   source =  "../s3_to_newrelic_connector_lambda.zip" 
 }
@@ -41,14 +41,14 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
 
 resource "aws_iam_role_policy_attachment" "s3_policy" {
   role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
 
 
 
 resource "aws_lambda_function" "lambda" {
   function_name = var.lambda_function_name
-  s3_bucket     = aws_s3_bucket.newrelic_lambda_bucket.bucket
+  s3_bucket     = aws_s3_bucket.kpp_lb_and_lambda_bucket.bucket
   s3_key        = aws_s3_bucket_object.lambda_zip.key
   handler       = "handler.lambda_handler"
   runtime       = "python3.11"
@@ -64,22 +64,44 @@ resource "aws_lambda_function" "lambda" {
 
 # Add S3 bucket notification to trigger Lambda
 resource "aws_s3_bucket_notification" "example_notification" {
-  bucket = aws_s3_bucket.newrelic_lambda_bucket.id
+  bucket = aws_s3_bucket.kpp_lb_and_lambda_bucket.id
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.lambda.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "logs/"
+    filter_prefix       = "AWSLogs/"
   }
 
-  depends_on = [aws_lambda_permission.example_permission]
+  depends_on = [aws_lambda_permission.s3_invoke_lambda_permission]
 }
 
 # Give S3 permission to invoke the Lambda function
-resource "aws_lambda_permission" "example_permission" {
+resource "aws_lambda_permission" "s3_invoke_lambda_permission" {
   statement_id  = "AllowS3InvokeLambda"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda.function_name
   principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.newrelic_lambda_bucket.arn
+  source_arn    = aws_s3_bucket.kpp_lb_and_lambda_bucket.arn
 }
+
+# Give ALB permission to write to s3 bucket
+resource "aws_s3_bucket_policy" "alb_logs_policy" {
+  bucket = aws_s3_bucket.kpp_lb_and_lambda_bucket.id              
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AWSLogDeliveryWrite",
+        Effect    = "Allow",
+        Principal = {
+          Service = "elasticloadbalancing.amazonaws.com"
+        },
+        Action    = "s3:PutObject",
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.kpp_lb_and_lambda_bucket.bucket}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+      }
+    ]
+  })
+}
+
+data "aws_caller_identity" "current" {}
